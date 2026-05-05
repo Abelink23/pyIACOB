103f29283b1120ab99a9a23a4b0a1d433827929b
Abel de Burgos Sierra <abel.deburgossierra@eso.org>
2026-05-05 17:21:06 -0400

    Add print_colors module: define color class for terminal text formatting

---
+++ print_colors.py
@@ -0,0 +1,18 @@
+# This file contains the class color, which is use in the
+# pyIACOB package to print colored text in the terminal.
+
+from os import error
+
+
+class color:
+    p = '\033[95m'
+    c = '\033[96m'
+    b = '\033[94m'
+    g = '\033[92m'
+    y = '\033[93m'
+    r = '\033[91m'
+    bold = '\033[1m'
+    under = '\033[4m'
+    end = '\033[0m'
+    error = r + 'ERROR: '
+    warn = y + 'WARNING: '
