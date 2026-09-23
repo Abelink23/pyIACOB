d717b7353dd1a7a35d30f036e179591024e75c54
Abel de Burgos Sierra <abel.deburgossierra@eso.org>
2026-09-23 16:57:46 -0300

    Refactor track functions: update trackmist and trackgene26 to improve parameter handling and add model options

--- tracks.py
+++ tracks.py
@@ -86,34 +86,49 @@ def trackmist(mass=None, metallicity=0.014, vini_vcrit=0.4, av=0.0):
         Enter the metallicity value as 0.###. Default is 0.014 (solar)
 
     vini_vcrit : float, optional
-        Enter the initial v/v_crit value [0.0/0.4]. Default is 0.4.
+        Enter the initial v/v_crit value. Default is 0.4.
+        Available values: 0.0, 0.4.
 
     av : float, optional
-        Enter the extinction (Av) of the isochrone to retrieve. Default is 1.0.
+        Enter the extinction (Av) of the isochrone to retrieve. Default is 0.0.
+        Available values: 0.0
 
     Returns
     -------
     MIST track.
     '''
 
+    av_list = [0.0]
+    vini_vcrit_list = [0.0, 0.4]
+    mass_list = [9, 12, 15, 20, 25, 30, 35, 40, 45, 60, 85]
+
+    # Choose Av
+    if not av in av_list:
+        msg.warn('Selected MIST Av not within: %s' % av_list)
+        av = min(av_list, key=lambda x:abs(x-av))
+        msg.info('Choosing %s as the closest value.' % av)
     if av < 1.0:
+        print(av)
         Av = str(av).replace('.','')
     else:
         Av = int(av*10)
 
+    # Choose vini_vcrit
+    if not vini_vcrit in vini_vcrit_list:
+        msg.warn('Selected MIST vini/vcrit not within: %s' % vini_vcrit_list)
+        vini_vcrit = min(vini_vcrit_list, key=lambda x:abs(x-vini_vcrit))
+        msg.info('Choosing %s as the closest value.' % vini_vcrit)
     vini_vcrit = str(vini_vcrit).replace('.','')
 
-    mass_list = [9, 12, 15, 20, 25, 30, 35, 40, 45, 60, 85]
-
+    # Choose mass
     if type(mass) is not str and not mass in mass_list:
-        msg.warn('Mass %s not in list %s' % (str(mass),mass_list))
+        msg.warn('Selected MIST mass not within: %s' % mass_list)
         mass = min(mass_list, key=lambda x:abs(x-mass))
         msg.info('Choosing %s as the closest value.' % mass)
-
     mass = str(int(mass)); digit = 3-len(mass)
     mass = '0'*digit+mass
 
-    metallicity = str(metallicity)[2:]
+    metallicity = str(metallicity)[3:]
 
     trk_mist = Table.read(tracksdir + 'MIST/M%sZ%sV%sAv%s.fits' % \
         (mass, metallicity, vini_vcrit, Av), format='fits')
@@ -121,7 +136,7 @@ def trackmist(mass=None, metallicity=0.014, vini_vcrit=0.4, av=0.0):
     return trk_mist
 
 
-def trackgene12(mass=None, vini_vcrit=0.4):
+def trackgene12(mass, vini_vcrit=0.4):
     '''
     Function to retrieve a specific track from Ekstrom et al. (2012).
 
@@ -130,9 +145,8 @@ def trackgene12(mass=None, vini_vcrit=0.4):
 
     Parameters
     ----------
-    mass : int/float, optional
+    mass : int/float
         Enter the mass in M/M_sun of the track you want to retrieve.
-        If None as input, all the tracks will be selected.
 
     vini_vcrit : float, optional
         Enter the initial v/v_crit value [0.0/0.2/0.4]. Default is 0.4.
@@ -158,24 +172,23 @@ def trackgene12(mass=None, vini_vcrit=0.4):
     400: last model
     '''
 
-    if mass == None:
-        msg.error('No mass given. Please enter a mass in M/M_sun.')
-        return
+    vini_vcrit_list = [0.0, 0.2, 0.4]
 
-    if not vini_vcrit in [0.0, 0.2, 0.4]:
-        msg.warn('Geneva tracks are only available for v/vcrit = 0.0, 0.2, and 0.4')
-        vini_vcrit = min([0.0, 0.2, 0.4], key=lambda x:abs(x-vini_vcrit))
+    # Choose vini_vcrit
+    if not vini_vcrit in vini_vcrit_list:
+        msg.warn('Selected Genec vini/vcrit not within: %s' % vini_vcrit_list)
+        vini_vcrit = min(vini_vcrit_list, key=lambda x:abs(x-vini_vcrit))
         msg.info('Choosing %s as the closest value.' % vini_vcrit)
 
     if vini_vcrit in [0.0, 0.4]:
         mass_list = [0.8, 0.9, 1.0, 1.1, 1.25, 1.35, 1.5, 1.7, 2.0, 2.5, 3, 4, 5, 7, 9, 12, 15, 20, 25, 32, 40, 60, 85, 120]
     elif vini_vcrit == 0.2:
         mass_list = [20, 25, 32, 40, 60, 85, 120]
-
     vini_vcrit = str(vini_vcrit).replace('0.','')
 
+    # Choose mass
     if type(mass) is not str and not mass in mass_list:
-        msg.warn('Mass not in list %s' % mass_list)
+        msg.warn('Selected Genec mass not within: %s' % mass_list)
         mass = min(mass_list, key=lambda x:abs(x-mass))
         msg.info('Choosing %s as the closest value.' % mass)
 
@@ -188,7 +201,7 @@ def trackgene12(mass=None, vini_vcrit=0.4):
     digit = 3-len(mass)
     mass = '0'*digit+mass
 
-    trk_geneva = Table.read(tracksdir + 'Ekstrom12/M%sZ14V%s.dat' % (mass,vini_vcrit), format='ascii', data_start=2, delimiter=' ')
+    trk_geneva = Table.read(tracksdir + 'Ekstrom12/M%sZ14V%s.fits' % (mass,vini_vcrit), format='fits')
 
     # FROM Gonzalo
     trk_geneva.rename_columns(['lg(Teff)','lg(L)'],['log_Teff','log_L'])
@@ -202,43 +215,60 @@ def trackgene12(mass=None, vini_vcrit=0.4):
     return trk_geneva
 
 
-def trackgene26(mass=None, vini_vcrit=0.4):
+def trackgene26(mass, vini_vcrit=0.4, model='magn', tams='FS'):
     '''
     Function to retrieve a specific track from Sciarini et al. (2026).
 
     NOTE: Tracks and isochrones from Geneva are downloaded from:
-          https://zenodo.org/records/18302392/
+          https://zenodo.org/records/22082876/
 
     Parameters
     ----------
-    mass : int/float, optional
+    mass : int/float
         Enter the mass in M/M_sun of the track you want to retrieve.
-        If None as input, all the tracks will be selected.
 
     vini_vcrit : float, optional
         Enter the initial v/v_crit value [0.0/0.2/0.4]. Default is 0.4.
 
+    model : str, optional
+        Enter 'magn' to retrieve a magnetic model. Alternatively, use
+        'hydro' to retrieve a hydrostatic model. Default is 'magn'.
+
+    tams : str, optional
+        Enter 'FS' if you want to retrieve a track computed to match the density
+        drop of the Full-Sample in de Burgos et al. (2025). Alternatively,
+        enter 'SR' to retrieve a track computed to much the density drop of the
+        Slow Rotating sample in de Burgos et al. (2025). Default is FS.
+
     Returns
     -------
     Geneva track.
+
+    Notes 'line' column
+    -------------------
+    1: ZAMS
+    2-84: H burning
+    85: minimum of Teff on the MS (main sequence hook)
+    86-109: H burning
+    110: Turn-off
+    111-189: HRD crossing and/or pre-He-burning core contraction
+    190-400: last computed model (the models correspond to the limit of the de Burgos+25
+            sample they are stopped once the effective temperature drops below 14 kK).
     '''
 
-    if mass == None:
-        msg.error('No mass given. Please enter a mass in M/M_sun.')
-        return
+    vini_vcrit_list = [0.0, 0.05, 0.10, 0.15, 0.25, 0.40, 0.60]
+    mass_list = [12, 15, 20, 25, 32, 40]
 
-    if not vini_vcrit in [0.0, 0.4]:
-        msg.warn('Geneva tracks are only available for v/vcrit = 0.0, 0.2, and 0.4')
-        vini_vcrit = min([0.0, 0.4], key=lambda x:abs(x-vini_vcrit))
+    # Choose vini_vcrit
+    if not vini_vcrit in vini_vcrit_list:
+        msg.warn('Selected Genec vini/vcrit not within: %s' % vini_vcrit_list)
+        vini_vcrit = min(vini_vcrit_list, key=lambda x:abs(x-vini_vcrit))
         msg.info('Choosing %s as the closest value.' % vini_vcrit)
+    vini_vcrit = str(vini_vcrit*10).replace('.','')
 
-    if vini_vcrit in [0.0, 0.4]:
-        mass_list = [10, 15, 20, 30, 45, 60]
-
-    vini_vcrit = str(vini_vcrit).replace('0.','0')
-
+    # Choose mass
     if type(mass) is not str and not mass in mass_list:
-        msg.warn('Mass not in list %s' % mass_list)
+        msg.warn('Selected Genec mass not within: %s' % mass_list)
         mass = min(mass_list, key=lambda x:abs(x-mass))
         msg.info('Choosing %s as the closest value.' % mass)
 
@@ -251,13 +281,20 @@ def trackgene26(mass=None, vini_vcrit=0.4):
     digit = 3-len(mass)
     mass = '0'*digit+mass
 
-    trk_geneva = Table.read(tracksdir + 'Sciarini26/M%sZ014V%sAv00.fits' % (mass,vini_vcrit), format='fits')
+    if model not in ['magn','hydro']:
+        msg.error("The model input parameter must be 'magn' or 'hydro'")
+    if tams not in ['FS','SR']:
+        msg.error("The tams input parameter must be 'FS' or 'SR'")
 
-    trk_geneva['Teff'] = (10**trk_geneva['logTeff'])/1e4
-    trk_geneva['log_LLsol'] = trk_geneva['logL'] - np.log10(trk_geneva['mass']) # NOT SURE ABOUT THIS ONE
-    trk_geneva['log_g'] = 4*trk_geneva['logTeff'] + np.log10(trk_geneva['mass']) - trk_geneva['logL'] - 10.61
-    trk_geneva['log_Lspec'] = 4*trk_geneva['logTeff'] - trk_geneva['log_g'] - 10.61
-    trk_geneva['He'] = trk_geneva['sHe4']/4/trk_geneva['sH1']
+    trk_geneva = Table.read(tracksdir + 'Sciarini26/M%sZ14V%sAv00_%s_%s.fits' % (mass,vini_vcrit,model,tams), format='fits')
+
+    trk_geneva.rename_columns(['lg(Teff)','lg(L)'],['log_Teff','log_L'])
+    #trk_geneva['L'] = (10**trk_geneva['lg(L)'])
+    trk_geneva['Teff'] = (10**trk_geneva['log_Teff'])/1e4
+    trk_geneva['log_LLsol'] = trk_geneva['log_L'] - np.log10(trk_geneva['mass']) # NOT SURE ABOUT THIS ONE
+    trk_geneva['log_g'] = 4*trk_geneva['log_Teff'] + np.log10(trk_geneva['mass']) - trk_geneva['log_L'] - 10.61
+    trk_geneva['log_Lspec'] = 4*trk_geneva['log_Teff'] - trk_geneva['log_g'] - 10.61
+    trk_geneva['He'] = trk_geneva['4He_surf']/4/trk_geneva['1H_surf']
 
     return trk_geneva
 
